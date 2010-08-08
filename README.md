ce633fdac5cdebfdf9580714fd844a39b5e10aae
Julian Kornberger <github.corny@digineo.de>
2010-08-08 21:29:17 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Liquid Markup Plugin for CKEditor 3.x
+
+This plugin highlights Liquid Markup Blocks like `{{variable.name}}` and `{% liquid block %}` in the WYSIWYG view.
+
+## Installation
+
+Copy the `plugin.js` to `/ckeditor/plugins/liquid/` and active it in your configuration (i.e. `/ckeditor/config.js`):
+
+    CKEDITOR.editorConfig = function( config ){
+      config.extraPlugins += (config.extraPlugins.length == 0 ? '' : ',') + 'liquid';
+    }
+
+## TODO
+
+* Write-project highlighted blocks in WYSIWYG-View
+* Auto-select blocks in WYSIWYG-View on single click
+
+## Sources
+
+* [CKeditor](http://ckeditor.com/)
+* [Liquid markup language](http://github.com/tobi/liquid)
---
+++ plugin.js
@@ -0,0 +1,55 @@
+CKEDITOR.plugins.add('liquid', {
+	requires: ['htmldataprocessor'],
+	afterInit: function(editor){
+		editor.addCss('liquidVariable, liquidBlock' +
+		'{' +
+			'color: #000;' +
+			'background-color: #ffc;' +
+			'border: 1px solid #ccc;' +
+			'padding: 2px;' +
+			'white-space:nowrap;' +
+		'}\n' +
+		'liquidBlock' +
+		'{' +
+			'background-color: #fcc;' +
+		'}');
+		
+		var dataProcessor = editor.dataProcessor, dataFilter = dataProcessor && dataProcessor.dataFilter, htmlFilter = dataProcessor && dataProcessor.htmlFilter;
+		
+		// html to data conversion
+		if (dataFilter) {
+			dataFilter.addRules({
+			
+				text: function(text){
+					text = text.replace(/\{\{([\w\.]+)\}\}/g, "<liquidVariable>$1</liquidVariable>");
+					text = text.replace(/\{%(.+)%\}/g, "<liquidBlock>$1</liquidBlock>");
+					return text;
+				}
+			});
+		}
+		
+		// data to html conversion
+		if (htmlFilter) {
+			htmlFilter.addRules({
+				elements: {
+					liquidvariable: function(element, b, c){
+						// Drop the wrapper element. 
+						delete element.name;
+						
+						if (element.children.size() > 0) {
+							element.children[0].value = "{{" + element.children[0].value + "}}"
+						}
+					},
+					liquidblock: function(element, b, c){
+						// Drop the wrapper element. 
+						delete element.name;
+						
+						if (element.children.size() > 0) {
+							element.children[0].value = "{%" + element.children[0].value + "%}"
+						}
+					}
+				}
+			});
+		}
+	},
+});
